a6e42cdb9e34942574e332ecad124adb6eb64373
Rubielena López <133280083+Fullsun15@users.noreply.github.com>
2023-09-30 18:04:54 -0400

    Update destruir_sesion.php

--- destruir_sesion.php
+++ destruir_sesion.php
@@ -1,10 +1,8 @@
 <?php
 session_start();
 
-// Destruir la sesión
 session_destroy();
 
-// Redirigir a index.php o a cualquier otra página después de destruir la sesión
 header('Location: index.php');
 exit();
 ?>
